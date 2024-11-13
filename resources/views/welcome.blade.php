dec3bd9a6b64933f981dff27d46b0ddc7b2ca8d5
hsan-hachicha-isetsf <hsan.hachicha@gmail.com>
2024-11-13 14:56:16 +0100

    Commit new version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link href="{{ secure_asset('frontend/assets/index-CPubGRoS.css') }}" rel="stylesheet">
+<title>Mon application Laravel</title>
 
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-
+requests">
+
+<link href="{{ secure_asset('frontend/assets/index-CPubGRoS.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1>Hello world</h1>
-    <div id="root"></div>
-<script src="{{ secure_asset('frontend/assets/index-C35M33Bu.js')}}"></script>
+<div id="root"></div>
+<script src="{{ secure_asset('frontend/assets/index-C35M33Bu.js') }}"></script>
 </body>
 </html>
