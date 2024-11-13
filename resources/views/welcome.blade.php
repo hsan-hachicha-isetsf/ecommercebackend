8a8dc4ca3a62fa8fe6342aa7a19fb905a38c21bc
hsan-hachicha-isetsf <hsan.hachicha@gmail.com>
2024-11-13 11:33:23 +0100

    Commit new version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link href="{{ secure_asset('frontend/assets/index-CPubGRoS.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <h1>Hello world</h1>
+    <div id="root"></div>
+<script src="{{ secure_asset('frontend/assets/index-C35M33Bu.js')}}"></script>
 </body>
 </html>
